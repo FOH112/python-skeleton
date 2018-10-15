93e073921d2f6e6688240fea96ab16367f67394c
FOH112 <44168739+FOH112@users.noreply.github.com>
2018-10-15 16:33:43 +0100

    Update q1.py

--- q1.py
+++ q1.py
@@ -4,6 +4,16 @@ import numpy as np
 
 # modify this function, and create other functions below as you wish
 def question01(portfolios):
-  # modify and then return the variable below
-  answer = 0
+# modify and then return the variable below
+  list_1 = []
+  list_2 = []
+
+  for i in portfolios:
+      for j in portfolios:
+          a = bin(i)
+          y = int(i)^int(j)
+          list_1.append(bin(y)[2:].zfill(len(a)))
+  for i in list_1:
+      list_2.append(int(i,2))
+  answer = max(list_2)
   return answer
